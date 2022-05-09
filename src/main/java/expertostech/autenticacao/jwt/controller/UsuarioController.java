c76692321c73471f8354139ca406d6bfe2014d7a
giovaner10 <giovanerr10@gmail.com>
2022-05-09 18:53:01 -0300

    controller de usuario com as alteracoes devidas para estudos

--- src/main/java/expertostech/autenticacao/jwt/controller/UsuarioController.java
+++ src/main/java/expertostech/autenticacao/jwt/controller/UsuarioController.java
@@ -1,11 +1,13 @@
 package expertostech.autenticacao.jwt.controller;
 
+import expertostech.autenticacao.jwt.model.Perfil;
 import expertostech.autenticacao.jwt.model.UsuarioModel;
 import expertostech.autenticacao.jwt.repository.UsuarioRepository;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +16,7 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping()
 public class UsuarioController {
 
     private final UsuarioRepository repository;
@@ -32,9 +34,13 @@ public class UsuarioController {
     }
 
     @PostMapping("/salvar")
-    public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario) {
-        usuario.setPassword(encoder.encode(usuario.getPassword()));
-        return ResponseEntity.ok(repository.save(usuario));
+    public ResponseEntity<UsuarioModel> salvar(/*@RequestBody UsuarioModel usuario*/) {
+
+        UsuarioModel model = new UsuarioModel("gio", "123");
+        model.setPassword(encoder.encode(model.getPassword()));
+
+        model.addPerfil(Perfil.ADMIN);
+        return ResponseEntity.ok(repository.save(model));
     }
 
     @GetMapping("/validarSenha")
@@ -52,4 +58,21 @@ public class UsuarioController {
         HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
         return ResponseEntity.status(status).body(valid);
     }
+
+
+    @PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/user")
+    public ResponseEntity<Boolean> roleUser() {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<Boolean> roleAdmin() {
+
+        return ResponseEntity.ok().build();
+    }
+
+
 }
